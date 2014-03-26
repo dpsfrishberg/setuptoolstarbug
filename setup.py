4319c2c6b53df47fbd45216035ffd15b5b91de8c
frishber <frishber@ncbi.nlm.nih.gov>
2014-03-26 17:00:32 -0400

    Added files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Setuptools Tar Bug",
+    version="1.0",
+    description="Demonstration of a bug with setuptools", 
+    author="Daniel Frishberg",
+    packages=find_packages(),
+)
